0fa2b4a81d3be1cbb3b7615e920f5a89476f1f8a
Steve <swanderson79@gmail.com>
2025-02-10 12:03:53 -0500

    Added header, fixed global CSS

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header id="header-container">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+#header-container {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background: red;
+  text-align: center;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  height: 2.5em;
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+router-link {
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+}
+
+router-link:hover {
+  color: blue;
+}
+</style>
