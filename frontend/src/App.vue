2058176ee49a671dfc93040a0e0a39676630b059
Steve <swanderson79@gmail.com>
2025-02-28 13:54:53 -0500

    Speed Insights

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
-import SpeedInsights from "@vercel/speed-insights";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
   <div id="app-container">
     <Header> </Header>
     <router-view> </router-view>
-    <SpeedInsights />
   </div>
+  <SpeedInsights />
 </template>
 
 <style scoped>
