a9d9a2f023611dd4c47d5da5e26222c7e0d4fa6d
Steve <swanderson79@gmail.com>
2025-03-07 10:38:36 -0500

    Adding CORs policy

--- backend/src/createApp.ts
+++ backend/src/createApp.ts
@@ -4,9 +4,19 @@ import testRouter from "./routes/testRoute";
 import employmentHistoryRouter from "./routes/employmentHistoryRoute";
 import educationRouter from "./routes/educationRoute";
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5173", // Local Vue dev server
+    "https://portfolio-project-iqotm9flk-steve-andersons-projects-d862a8e0.vercel.app/", // Preview deployment
+    "https://steveandersonthedeveloper.com", // Production deployment
+  ],
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
 export const createApp = () => {
   const app = express();
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use("/test-db", testRouter);
   app.use("/employment-history", employmentHistoryRouter);
   app.use("/education", educationRouter);
